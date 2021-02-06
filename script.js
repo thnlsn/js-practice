4f476997d627389a7d42571f8e895034bae8688e
Thomas Nelson <thnlsn@gmail.com>
2021-02-06 12:30:16 -0800

    practicing with the set datastructure in javascript

--- script.js
+++ script.js
@@ -1251,7 +1251,7 @@ for (const [day, { open, close }] of entries) {
   );
 } */
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-const game = {
+/* const game = {
   team1: 'Bayern Munich',
   team2: 'Borrussia Dortmund',
   players: [
@@ -1290,20 +1290,20 @@ const game = {
     x: 3.25,
     team2: 6.5,
   },
-};
+}; */
 
 // Let's continue with our football betting app!
 
 // 1. Loop over the game.scored array and print each player name to the console, along with the goal number (Example: "Goal 1: Lewandowski")
-for (const [goal, player] of game.scored.entries()) {
+/* for (const [goal, player] of game.scored.entries()) {
   console.log(`Goal ${goal + 1}: ${player}`);
 }
 console.log(
   '================================================================================================================'
-);
+); */
 
 // 2. Use a loop to calculate the average odd and log it to the console (We already studied how to calculate averages, you can go check if you don't remember)
-let avg = 0;
+/* let avg = 0;
 for (const odd of Object.values(game.odds)) {
   avg += odd;
 }
@@ -1311,20 +1311,20 @@ avg /= Object.values(game.odds).length;
 console.log(avg);
 console.log(
   '================================================================================================================'
-);
+); */
 
 // 3. Print the 3 odds to the console, but in a nice formatted way, exaclty like this:
 /*    Odd of victory Bayern Munich: 1.33
       Odd of draw: 3.25
       Odd of victory Borrussia Dortmund: 6.5 */
 // Get the team names directly from the game object, don't hardcode them (except for "draw"). HINT: Note how the odds and the game objects have the same property names 😉
-for (const [team, odd] of Object.entries(game.odds))
+/* for (const [team, odd] of Object.entries(game.odds))
   console.log(
     `Odd of ${game[team] ? 'victory' : 'draw'} ${game[team] ?? '' - ''}: ${odd}`
   );
 console.log(
   '================================================================================================================'
-);
+); */
 
 // BONUS: Create an object called 'scorers' which contains the names of the players who scored as properties, and the number of goals as the value. In this game, it will look like this:
 /*
@@ -1335,7 +1335,60 @@ console.log(
       }
 */
 //   scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
-const scorers = {};
+/* const scorers = {};
 for (const scorer of game.scored)
   scorers[scorer] ? scorers[scorer]++ : (scorers[scorer] = 1);
-console.log(scorers);
+console.log(scorers); */
+
+// Given 3 arguments of arrays, fill in all null values with numbers 1-9, however, you cannot reuse a number so each number 1-9
+
+/* const args = [
+  [null, 7, null],
+  [5, null, 3],
+  [null, null, null, null],
+];
+
+const unused = (nums) => {
+  for (const num of nums) {
+    if
+  }
+};
+
+// Deconstruct the
+const func = (...[args]) => {
+  let usedNums = [];
+  let availableNums = [];
+
+  // First collect numbers we cannot use (they are already in the args) into an array:
+  args.map((arr) => {
+    // Check if num exists, if so, push that value to usedNums
+    arr.forEach((num) => {
+      if (num !== null) usedNums.push(num);
+    });
+  });
+  console.log(usedNums);
+
+  // Put numbers
+};
+
+// Calling the func with a spread collection of arrays, the first represents the upper array, the second the lower, and the last the sum total
+func(args); */
+
+/* 
+        48 39 -1+1 --- 36 27 - 9
+        87 96 +1-1 --- 72 81 - 9
+        56 74 +2-2 --- 45 63 - 18
+        63 27 -4+4 --- xx xx - 36
+*/
+
+// SET: Contains no dupicate values
+const ordersSet = new Set([
+  'Pasta',
+  'Pizza',
+  'Pizza',
+  'Risotto',
+  'Pasta',
+  'Pizza',
+]);
+
+console.log(ordersSet);
