62b25d946dc781a080868734064aade388d4c29a
Thomas Nelson <thnlsn@gmail.com>
2021-02-08 00:35:44 -0800

    string problem practice

--- script.js
+++ script.js
@@ -1812,3 +1812,64 @@ capitalizeName('thomas harlon nelson'); */
 
 // const message = 'Go to gate 23!';
 // console.log(message.padStart(25, '|').padEnd(35, '|'));
+
+/* const maskCreditCard = function (number) {
+  const str = `${number}`;
+  console.log(str.slice(-4).padStart(str.length, '*'));
+};
+maskCreditCard('123454323456'); */
+
+// Repeat
+/* const message = 'Bad weather... All departures delayed. ';
+console.log(message.repeat(100)); */
+
+/* const planeCount = (count) => {
+  console.log(
+    `We have ${count} planes waiting in the bay: ${'✈️ '.repeat(count)}`
+  );
+};
+planeCount(10); */
+
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+document.querySelector('button').addEventListener('click', function () {
+  const text = document.querySelector('textarea').value;
+  const lower = text.toLowerCase().split('\n'); // Make everything lower case // Make an array of substrings of each line
+  for (let i = 0; i < lower.length; i++) {
+    const [first, second] = lower[i].trim().split('_');
+    const camelCase = `${first}${second.replace(
+      second[0],
+      second[0].toUpperCase()
+    )}`;
+    console.log(camelCase.padEnd(20) + '✅'.repeat(i + 1));
+  }
+});
+
+// Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+
+// The input will come from a textarea inserted into the DOM (see code below), and conversion will happen when the button is pressed.
+
+// THIS TEST DATA (pasted to textarea)
+/*
+underscore_case
+ first_name
+Some_Variable 
+  calculate_AGE
+delayed_departure
+*/
+
+// SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
+// underscoreCase      ✅
+// firstName           ✅✅
+// someVariable        ✅✅✅
+// calculateAge        ✅✅✅✅
+// delayedDeparture    ✅✅✅✅✅
+
+// HINT 1: Remember which character defines a new line in the textarea 😉
+// HINT 2: The solution only needs to work for a variable made out of 2 words, like a_b
+// HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
+// HINT 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
+
+// Afterwards, test with your own test data!
+
+// GOOD LUCK 😀
